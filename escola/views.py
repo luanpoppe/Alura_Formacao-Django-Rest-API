bf650149e863727b8380efa24ab74a4091df109c
luanpoppe <luanpoppe@gmail.com>
2024-01-03 18:37:10 -0300

    Terminei a aula 03-02 - Viewset

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,21 @@
-from django.http import JsonResponse
+# from django.http import JsonResponse
 
-def alunos(request):
-    if request.method == 'GET':
-        aluno = {'id': 1, 'nome': 'Guilherme'}
-        return JsonResponse(aluno)
+# def alunos(request):
+#     if request.method == 'GET':
+#         aluno = {'id': 1, 'nome': 'Guilherme'}
+#         return JsonResponse(aluno)
+
+from rest_framework import viewsets
+from escola.models import Aluno, Curso
+from serializer import AlunoSerializer, CursoSerializer
+
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os alunos e alunas"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+class CursosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os cursos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
