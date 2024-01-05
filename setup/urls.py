2e31f03fa2b86a7ed49379197cd349da58b88e45
luanpoppe <luanpoppe@gmail.com>
2024-01-05 19:58:49 -0300

    Terminei a aula 05-03 - Matrículas de um curso

--- setup/urls.py
+++ setup/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from escola.views import AlunosViewSet, CursosViewSet, MatriculaViewSet, ListaMatriculasAluno
+from escola.views import AlunosViewSet, CursosViewSet, ListaAlunosMatriculados, MatriculaViewSet, ListaMatriculasAluno
 from rest_framework import routers
 
 router = routers.DefaultRouter()
@@ -12,5 +12,6 @@ router.register("matriculas", MatriculaViewSet, basename="Matrículas")
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include(router.urls)),
-    path("aluno/<int:pk>/matriculas/", ListaMatriculasAluno.as_view())
+    path("aluno/<int:pk>/matriculas/", ListaMatriculasAluno.as_view()),
+    path("curso/<int:pk>/matriculas/", ListaAlunosMatriculados.as_view())
 ]
