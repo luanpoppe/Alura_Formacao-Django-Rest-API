c979d2c3f4d050db43fd37465cdc59147f5bf6a9
luanpoppe <luanpoppe@gmail.com>
2024-01-06 20:08:13 -0300

    Terminei a aula 03-02 - Expressões regulares

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -14,10 +14,7 @@ class ClienteSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError({"nome":"Não inclua números neste campo"})
         if not rg_valido(data["rg"]):
             raise serializers.ValidationError({"rg": "O RG deve ter 9 dígitos"})
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({"celular": "O número de celular deve seguir o este modelo: 11 91234-1234, respeitando os espaços e traço."})
         return data
     
-
-    # def validate_celular(self, celular):
-    #     if len(celular) < 11:
-    #         raise serializers.ValidationError("O celular deve ter 11 dígitos")
-    #     return celular
--- clientes/validators.py
+++ clientes/validators.py
@@ -1,3 +1,5 @@
+import re
+
 def cpf_valido(numero_do_cpf):
     return len(numero_do_cpf) == 11
 
@@ -6,3 +8,9 @@ def nome_valido(nome):
 
 def rg_valido(numero_do_rg):
     return len(numero_do_rg) == 9
+
+def celular_valido(numero_celular):
+    """Verifica se o celular é válido (11 91234-123)"""
+    modelo = '[0-9]{2} [0-9]{5}-[0-9]{4}'
+    resposta = re.findall(modelo, numero_celular)
+    return resposta
