12c4ac6fd6b1ec3ed66b049d5d97be0befe841d6
luanpoppe <luanpoppe@gmail.com>
2024-01-06 20:20:43 -0300

    Terminei a auala 03-03 - Importando validações

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -9,7 +9,7 @@ class ClienteSerializer(serializers.ModelSerializer):
     
     def validate(self, data):
         if not cpf_valido(data['cpf']):
-            raise serializers.ValidationError({"cpf":"O CPF deve ter 11 dígitos"})
+            raise serializers.ValidationError({"cpf":"Número de CPF inválido"})
         if not nome_valido(data['nome']):
             raise serializers.ValidationError({"nome":"Não inclua números neste campo"})
         if not rg_valido(data["rg"]):
